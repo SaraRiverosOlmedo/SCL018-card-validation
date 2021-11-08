9ca0992353bb1ac1759eb14c0581c86661595369
Sara Riveros <s.riveros.olmedo@gmail.com>
2021-11-07 21:49:25 -0300

    Diseño en CSS, uso de HTML semantico y Uso de selectores de DOM en el index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const codecard= document.getElementById("codeinput");
+codecard.addEventListener()
+
+const selectcard= document.getElementById("optionsCards");
+selectcard.addEventListener("change",(event)=>{
+const 
+}
+)
+const buttonValidations= document.getElementById("btnValidation");

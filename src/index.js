b9501be684b82519e88fab3ba85d8ab6de4f54be
SaraRiverosOlmedo <s.riveros.olmedo@gmail.com>
2021-11-22 18:26:53 -0300

    se agrego las funciones de validar y maskify

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-import validator from './validator.js';
-
-console.log(validator);
-
-const codecard= document.getElementById("codeinput");
-codecard.addEventListener("")
-
-const selectcard= document.getElementById("optionsCards");
-selectcard.addEventListener("change",(event)=>{
-const 
-}
-)
-const buttonValidations= document.getElementById("btnValidation");
-buttonValidations.addEventListener("click",(event)=>{
-
-})
+import validator from "./validator.js";
+   document.getElementById("btnValidation").addEventListener("click", checkIn); 
+   function checkIn() {
+     const boxContent = document.getElementById("creditnumber").value; 
+     let contentToValidate = boxContent.toString(); 
+     if (contentToValidate.match("^[0-9]{16}$")) { 
+         alert("Estamos procesando tu solicitud"); 
+           if (validator.isValid(contentToValidate)) {
+           alert("Genial, está validada y tu tarjeta N°" + validator.maskify(toValidations) + " se ha registrado exitosamente"
+           );
+          } else {
+           alert("El número ingresado no es válido. Verifica que ingresaste los dígitos correctamente o intenta nuevamente con otro documento");
+           }
+      } else {
+        alert("Debes ingresar un número de 16 dígitos");
+        throw TypeError("Only numbers allowed");
+     }
+  }
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
+  isValid(toValidations){
+      let digitsForLuhn = toValidations.split ("").reverse();                    
+      let evenPosition = []; 
+      let oddPosition = []; 
+          for (let i = 0; i < digitsForLuhn.length;i++){ 
+               if ((i+1) % 2 === 0){
+               evenPosition.push(digitsForLuhn[i]);
+              } else {
+               oddPosition.push(digitsForLuhn[i]);
+              }
+           }
+      let duplicatedDigits = []; 
+          evenPosition.forEach (item => {
+          duplicatedDigits.push(item*2);
+          });
+      let twoDigits = duplicatedDigits.filter(item => item > 9);
+      let oneDigit = duplicatedDigits.filter(item => item <= 9);
+      let twoDigitsSumed = []; 
+          twoDigits.forEach (item => {
+          twoDigitsSumed.push(item-9);
+          });
+      let allToIntegers = (twoDigitsSumed.concat(oneDigit,oddPosition)).map(Number); 
+      let totalSum=0; 
+      for(let i of allToIntegers) totalSum+=i; 
+          if (totalSum % 10 === 0) { 
+          return true; 
+      } else {
+          return false; 
+        }   
+      },                 
+  maskify (toValidations){
+      return toValidations.slice(0,-3).replace(/.(?=.)/g, '#').concat(toValidations.slice(-3, toValidations.length));   
+  }
 };
-
 export default validator;
